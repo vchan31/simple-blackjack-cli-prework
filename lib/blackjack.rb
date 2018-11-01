2f238321be40e3324c6045a5ebd4504d72cc9256
sensible-proc-7557 <vchan313@gmail.com>
2018-11-01 18:25:30 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,39 +1,57 @@
 def welcome
-  # code #welcome here
+  puts 'Welcome to the Blackjack Table'
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  answer = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  deal_1 = deal_card
+  deal_2 = deal_card
+  card_total = deal_1 + deal_2
+  display_card_total(card_total)
+    return card_total
 end
 
-def hit?
-  # code hit? here
+
+def hit?(int)
+  prompt_user
+  answer = get_user_input
+  
+  if answer == "s"
+    return int
+  elsif answer == 'h'
+    deal_1 = deal_card
+    return deal_1 + int
+  else
+    invalid_command
+    return int 
+  end
+  
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
-
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
