5745e9f5829f85d5aef8971538cea5a3655dc0d9
sensible-proc-7557 <vchan313@gmail.com>
2018-11-01 19:02:10 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,75 @@
 def welcome
-  # code #welcome here
+  puts 'Welcome to the Blackjack Table'
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  answer = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  deal_1 = deal_card
+  deal_2 = deal_card
+  card_total = deal_1 + deal_2
+  display_card_total(card_total)
+    return card_total
 end
 
-def hit?
-  # code hit? here
+
+def hit?(int)
+  prompt_user
+  answer = get_user_input
+  
+  if answer == "s"
+    return int
+  elsif answer == 'h'
+    deal_1 = deal_card
+    return deal_1 + int
+  else
+    invalid_command
+    return int 
+  end
+  
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
+
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  #first, they are welcome'd
+  welcome
+  #then they will be dealt two cards in the initial round, the value is saved in running_total,  
+  running_total = initial_round
+  #Use a until loop, and ask if they wanna hit or stay using hit? function:
+  until running_total > 21
+    running_total = hit?(running_total)
+    display_card_total(running_total)
+    
+  end
+  #Once the loop stops, the game ending function runs telling them the full hand, 
+  end_game(running_total)
 end
     
